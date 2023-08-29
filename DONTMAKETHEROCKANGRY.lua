3e20e0e8bfab47cfef2dd7e21a4947284531a05e
susy <110405809+xxsfytd@users.noreply.github.com>
2023-08-29 20:34:57 +0800

    Add files via upload

---
+++ DONTMAKETHEROCKANGRY.lua
@@ -0,0 +1,52 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Dont Make Rock Angry", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddLabel("before turning on the auto teleport")
+Tab:AddLabel("click the Platform First")
+
+Tab:AddToggle({
+	Name = "Auto Teleport",
+	Default = false,
+	Callback = function(Value)
+	_G.autoTeleport = Value
+	while _G.autoTeleport and wait() do
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(504, 128, 1831)
+		end
+	end    
+})
+
+Tab:AddButton({
+	Name = "Platform",
+	Callback = function()
+	
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(504, 128, 1831)
+	
+      		-- Get a reference to the LocalPlayer
+local player = game.Players.LocalPlayer
+
+-- Create a new part
+local part = Instance.new("Part")
+part.Size = Vector3.new(5, 5, 5) -- Adjust the size as needed
+part.BrickColor = BrickColor.new("Bright blue") -- Adjust the color as needed
+part.Anchored = true
+part.Position = player.Character.HumanoidRootPart.Position + Vector3.new(0, -10, 0) -- Adjust the position as needed
+
+-- Parent the part to the workspace
+part.Parent = game.Workspace
+
+-- Move the part below the player
+player.Character.HumanoidRootPart.CFrame = CFrame.new(player.Character.HumanoidRootPart.Position + Vector3.new(0, -10, 0))
+
+-- Adjust the camera to focus on the new part
+game:GetService("Workspace").CurrentCamera.CameraSubject = part
+
+  	end    
+})
+
+
